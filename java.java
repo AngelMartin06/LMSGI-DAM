747c8da23fc3f4df85940b8eac918d9f97c770a9
AngelMartin06 <domicxomic@alumnos.alborfp.com>
2025-11-03 16:10:13 +0100

    ejercicio65

---
+++ java.java
@@ -0,0 +1,73 @@
+import java.util.Scanner;
+
+public class java {
+
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+
+        /*
+         * int numero;
+         * System.out.print("Ingrese un numero entero: ");
+         * numero = leer.nextInt();
+         * while (numero <= 10) {
+         * System.out.println(numero);
+         * numero++;
+         */
+
+        /*
+         * int numero;
+         * int suma = 0;
+         * numero = leer.nextInt();
+         * while (numero <= 5) {
+         * suma = suma + numero1;
+         * System.out.println(numero);
+         * numero++;
+         * 
+         * }
+         * 
+         * 
+         * int numero;
+         * int contador = 0;
+         * int positivos = 0;
+         * int negativos = 0;
+         * System.out.println("Ingrese 5 numeros");
+         * while (contador < 5) {
+         * numero = leer.nextInt();
+         * contador++;
+         * if (numero >= 0) {
+         * positivos++;
+         * } else {
+         * negativos++;
+         * 
+         * }
+         * 
+         * }
+         * System.out.println("La suma de los numeros es: " + positivos +
+         * " positivos y " + negativos + " negativos");
+         * 
+         * int numero;
+         * System.out.println("ingrese el numero primo");
+         * numero = leer.nextInt();
+         * 
+         * if (numero % 2 == 0) {
+         * System.out.println("El numero no es primo");
+         * } else {
+         * System.out.println("El numero es primo");
+         * }
+         */
+        int numero;
+        System.out.println("ingrese un numero");
+        numero = leer.nextInt();
+        int suma = 0;
+        if (numero > 0) {
+
+            for (int i = 1; i <= 100; i++) {
+                suma += numero + i;
+                System.out.println(suma);
+            }
+        } else {
+            System.out.println("El numero es negativo");
+        }
+
+    }
+}
