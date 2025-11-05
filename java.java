627dd74214dbe8346c7247816a437cf66bbda59c
AngelMartin06 <domicxomic@alumnos.alborfp.com>
2025-11-05 15:58:35 +0100

    ejercicio76

--- java.java
+++ java.java
@@ -54,20 +54,48 @@ public class java {
          * } else {
          * System.out.println("El numero es primo");
          * }
+         * EJERCICIO1 16
+         * int numero;
+         * System.out.println("ingrese un numero");
+         * numero = leer.nextInt();
+         * int suma = 0;
+         * if (numero > 0) {
+         * 
+         * for (int i = 1; i <= 100; i++) {
+         * suma += numero + i;
+         * System.out.println(suma);
+         * }
+         * } else {
+         * System.out.println("El numero es negativo");
+         * }
+         * 
+         * int numero;
+         * int numero2;
+         * System.out.println("Ingrese dos numero");
+         * numero = leer.nextInt();
+         * numero2 = leer.nextInt();
+         * int suma1 = 0;
+         * int suma = 0;
+         * 
+         * for (int i = 1; i < 7; i++) {
+         * suma = numero + i;
+         * suma1 = numero2 + i;
+         * System.out.println(suma + " " + suma1);
+         * }
          */
-        int numero;
-        System.out.println("ingrese un numero");
-        numero = leer.nextInt();
-        int suma = 0;
-        if (numero > 0) {
 
-            for (int i = 1; i <= 100; i++) {
-                suma += numero + i;
-                System.out.println(suma);
+        int numero = 2;
+        System.out.println("Ingrese un numero");
+
+        if (numero % 2 == 0) {
+            for (int i = 2; i <= numero; i++) {
+                if (numero % i == 0 && numero != i) {
+                    System.out.println("El numero no es primo");
+                    break;
+                } else if (numero == i) {
+                    System.out.println("El numero es primo");
+                }
             }
-        } else {
-            System.out.println("El numero es negativo");
         }
-
     }
 }
