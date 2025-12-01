6ba7f41c615a65156df6fa7e32de9288947500e3
AngelMartin06 <domicxomic@alumnos.alborfp.com>
2025-12-01 10:51:18 +0100

    ejercicio107

--- javaejercicios.java
+++ javaejercicios.java
@@ -1,6 +1,30 @@
 
-import.util.array;
+import java.util.Scanner;
 
 public class javaejercicios {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numero = 0;
+        int suma = 0;
+
+        System.out.println("=== Suma Acumulativa ===");
+        System.out.println("Introduce números. Escribe 0 para terminar.\n");
+
+        do {
+            System.out.print("Introduce un número: ");
+            numero = scanner.nextInt();
+
+            if (numero != 0) {
+                suma += numero;
+                System.out.println("Suma parcial: " + suma);
+            }
+        } while (numero != 0);
+
+        System.out.println("\n=== Resultado Final ===");
+        System.out.println("Suma total: " + suma);
+
+        scanner.close();
+    }
 
 }
